b68564bfa74fd5fbad4e8995e6b02e021f183fe0
Vladislav Kapicyn <zveronym@gmail.com>
2022-05-12 11:53:37 +0300

    Fix audit issue

--- contracts/mocks/SuperproToken.sol
+++ contracts/mocks/SuperproToken.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SuperproToken is ERC20, ERC20Burnable {
+contract SuperproToken is ERC20 {
     constructor(
         uint256 supply,
         string memory ticker,
